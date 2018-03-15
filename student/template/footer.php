8b43fafcdb180515e4d2cd9e9a76ce7ccd6ab222
readiu <reads@central.edu>
2018-03-15 11:48:02 -0500

    Add files via upload

---
+++ student/template/footer.php
@@ -0,0 +1,19 @@
+			<hr />
+			<div class="clearfix"></div>
+			<div id="footer">
+				<a href="//www.central.edu">
+					<img src="//d1lqhpmxg10s5j.cloudfront.net/images/2015/centralCollegeLogoSmall.svg"
+						alt="Central College logo" style="height: 66px;">
+				</a>
+				<br />
+				<a href="//www.central.edu/policies/policy/678">Privacy Policy</a>
+			</div>
+		</div>
+	</div>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"
+		integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"
+		integrity="sha256-U5ZEeKfGNOja007MMD3YBI0A3OSZOQbeG6z2f2Y0hu8=" crossorigin="anonymous"></script>
+	<?=$this->bodyAppend?>
+</body>
+</html>
